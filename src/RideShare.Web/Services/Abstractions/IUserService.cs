3cefeaa01e6879511540df76c127b58e7ffb49be
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 14:04:31 +0300

    Implement service layer for user

---
+++ src/RideShare.Web/Services/Abstractions/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration.UserSecrets;
+using RideShare.Web.Dtos.Request;
+using RideShare.Web.Dtos.Response;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Services.Abstractions
+{
+    public interface IUserService
+    {
+        Task<UserResponseDto> RegisterUserAsync([NotNull]UserRequestDto dto);
+
+        Task<UserResponseDto> GetUserInformation([NotNull] string userId);
+
+        Task DeleteUserAsync([NotNull] string userId);
+
+        Task ReplaceUserAsync([NotNull] string userId, [NotNull] UserUpdateRequestDto dto);
+
+        Task ActiveUserAsync([NotNull]string userId);
+        Task PassiveUserAsync([NotNull] string userId);
+    }
+}
