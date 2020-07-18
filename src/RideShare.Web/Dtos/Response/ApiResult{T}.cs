c57b6d1221981a0687d28d63413f981378a1dcbf
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 13:07:58 +0300

    Add base response dtos

---
+++ src/RideShare.Web/Dtos/Response/ApiResult{T}.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RideShare.Web.Dtos.Response
+{
+    /// <summary>
+    /// This is base class of Api Response models. All requests will return in this template. See BaseController for implementation.
+    /// </summary>
+    /// <typeparam name="T">Type of data.</typeparam>
+    public class ApiResult<T> : ApiResult<T, object>
+    {
+    }
+}
