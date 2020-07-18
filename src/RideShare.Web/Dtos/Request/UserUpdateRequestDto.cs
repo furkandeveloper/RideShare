94468920c7eb9d4fa8ec96823ecdde2d8e37b2b0
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 14:04:13 +0300

    Implement validation rule for user

---
+++ src/RideShare.Web/Dtos/Request/UserUpdateRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Dtos.Request
+{
+    public class UserUpdateRequestDto : UserRequestDto
+    {
+    }
+}
---
+++ src/RideShare.Web/Validations/UserUpdateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using RideShare.Web.Dtos.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Validations
+{
+    public class UserUpdateValidator : AbstractValidator<UserRequestDto>
+    {
+        public const string regex = @"^[2-9]\d{2}-\d{3}-\d{4}$";
+        public UserUpdateValidator()
+        {
+            RuleFor(r => r.Name).NotEmpty().NotNull().When(r=>!string.IsNullOrEmpty(r.Name));
+            RuleFor(r => r.Surname).NotEmpty().NotNull().When(r => !string.IsNullOrEmpty(r.Surname));
+            RuleFor(r => r.PhoneNumber).NotEmpty().NotNull().Matches(regex).When(r => !string.IsNullOrEmpty(r.PhoneNumber));
+        }
+    }
+}
