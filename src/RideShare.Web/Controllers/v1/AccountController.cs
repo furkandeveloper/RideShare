e146804a683f0e8040269ede0e86038e7dfae4b6
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 13:23:08 +0300

    Add AccountController

---
+++ src/RideShare.Web/Controllers/v1/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class AccountController : BaseController
+    {
+
+    }
+}
