9909b0aeeb3123e7579035ad8ab3c9c8c4d132d0
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 17:18:41 +0300

    Configure object mapping

---
+++ src/RideShare.Web/Mapping/TravelMap.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using RideShare.Web.Dtos.Request;
+using RideShare.Web.Dtos.Response;
+using RideShare.Web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Mapping
+{
+    public class TravelMap : Profile
+    {
+        public TravelMap()
+        {
+            CreateMap<Travel, TravelResponseDto>().ReverseMap();
+
+            CreateMap<TravelRequestDto, Travel>().ReverseMap();
+
+            CreateMap<TravelUpdateRequestDto,Travel>().ForAllMembers(opt => opt.Condition((source, destination, member) => member != null));
+        }
+    }
+}
