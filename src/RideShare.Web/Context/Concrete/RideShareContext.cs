dee71a6a12b994055c72b2c71ee652dbd73569f4
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 13:37:10 +0300

    Implement Db Context

---
+++ src/RideShare.Web/Context/Concrete/RideShareContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using RideShare.Web.Context.Abstractions;
+using RideShare.Web.Entities;
+using RideShare.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Context.Concrete
+{
+    public class RideShareContext : DbContext, IRideShareContext
+    {
+        public RideShareContext(Settings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Database);
+        }
+
+        protected readonly IMongoDatabase _database = null;
+        /// <summary>
+        /// Root of Database object
+        /// </summary>
+        public new IMongoDatabase Database
+        {
+            get
+            {
+                return _database;
+            }
+        }
+
+        public IMongoCollection<User> Users
+            => Database.GetCollection<User>(nameof(Users));
+
+        public IMongoCollection<T> Set<T>(string collection = null)
+        {
+            return Database.GetCollection<T>(collection ?? typeof(T).Name);
+        }
+    }
+}
