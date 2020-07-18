69ac58b348e2c3fc1b5a0677be8a8ccfd4da2f2b
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 17:18:07 +0300

    Add travel and enum entities

---
+++ src/RideShare.Web/Entities/Travel.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class Travel : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string StartingPoint { get; set; }
+
+        public string TargetPoint { get; set; }
+
+        public int TotalArmchair { get; set; }
+
+        public int CurrentArmchair { get; set; }
+
+        public List<User> Users { get; set; } = new List<User>();
+    }
+}
