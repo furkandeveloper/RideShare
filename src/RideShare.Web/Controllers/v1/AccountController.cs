9bf9d59c3c5348f5a6187da9b39c772028d097cf
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 13:37:21 +0300

    Add token information

--- src/RideShare.Web/Controllers/v1/AccountController.cs
+++ src/RideShare.Web/Controllers/v1/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 namespace RideShare.Web.Controllers.v1
 {
     [ApiVersion("1.0")]
+    [AllowAnonymous]
     public class AccountController : BaseController
     {
 
