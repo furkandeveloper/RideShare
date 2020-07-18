c69f9b0b6f379e16dcd2c82ef70b6e2152424a7a
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 18:07:22 +0300

    Implement controller layer of unit test scenarios

---
+++ test/RideShare.UnitTest/Controller/AccountTest.cs
@@ -0,0 +1,178 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RideShare.UnitTest.Static;
+using RideShare.Web.Controllers.v1;
+using RideShare.Web.Dtos.Request;
+using RideShare.Web.Dtos.Response;
+using RideShare.Web.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RideShare.UnitTest.Controller
+{
+    public class AccountTest
+    {
+        private readonly string userId = Guid.NewGuid().ToString();
+
+        [Theory, AutoMoqData]
+        public async Task InsertBrandAsync_Return_Created_Result(Mock<IUserService> userService,
+                                                               UserResponseDto expected)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            UserRequestDto requestDto = new UserRequestDto() { Name = "testName", Surname = "testSurname"};
+            userService.Setup(setup => setup.RegisterUserAsync(requestDto)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.RegisterUserAsync(requestDto);
+
+            var apiResult = result.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            UserResponseDto response = model.Data as UserResponseDto;
+
+            // Assert
+
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<UserResponseDto>(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task GetBrandAsync_Return_Ok_Result(Mock<IUserService> userService,
+                                                               UserResponseDto expected)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            userService.Setup(setup => setup.GetUserInformationAsync(userId)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.GetUserInformationAsync(userId);
+
+            var apiResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            UserResponseDto response = model.Data as UserResponseDto;
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<UserResponseDto>(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task UpdateBrandAsync_Return_NoContent_Result(Mock<IUserService> userService)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            UserUpdateRequestDto dto = new UserUpdateRequestDto();
+            userService.Setup(setup => setup.ReplaceUserAsync(userId,dto));
+
+            // Act
+            var result = sut.UpdateUserAsync(userId,dto);
+
+
+            // Assert
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);            
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DeleteBrandAsync_Return_NoContent_Result(Mock<IUserService> userService)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            userService.Setup(setup => setup.DeleteUserAsync(userId));
+
+            // Act
+            var result = sut.DeleteUserAsync(userId);
+
+
+            // Assert
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ActiveBrandAsync_Return_NoContent_Result(Mock<IUserService> userService)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            userService.Setup(setup => setup.ActiveUserAsync(userId));
+
+            // Act
+            var result = sut.ActiveUserAsync(userId);
+
+
+            // Assert
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task PassiveBrandAsync_Return_NoContent_Result(Mock<IUserService> userService)
+        {
+            // Arrange
+            var sut = new AccountController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            userService.Setup(setup => setup.PassiveUserAsync(userId));
+
+            // Act
+            var result = sut.PassiveUserAsync(userId);
+
+
+            // Assert
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+    }
+}
---
+++ test/RideShare.UnitTest/Controller/TravelTest.cs
@@ -0,0 +1,242 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RideShare.UnitTest.Static;
+using RideShare.Web.Controllers.v1;
+using RideShare.Web.Dtos.Request;
+using RideShare.Web.Dtos.Response;
+using RideShare.Web.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RideShare.UnitTest.Controller
+{
+    public class TravelTest
+    {
+        private readonly string userId = Guid.NewGuid().ToString();
+        private readonly string travelId = Guid.NewGuid().ToString();
+
+        [Theory, AutoMoqData]
+        public async Task InsertTravelAsync_Return_Created_Result(Mock<ITravelService> userService,
+                                                               TravelResponseDto expected)
+        {
+            // Arrange
+            var sut = new TravelController(userService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            TravelRequestDto requestDto = new TravelRequestDto();
+            userService.Setup(setup => setup.InsertTravelAsync(requestDto)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.InsertTravelAsync(requestDto);
+
+            var apiResult = result.Result.Should().BeOfType<CreatedAtActionResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            TravelResponseDto response = model.Data as TravelResponseDto;
+
+            // Assert
+
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<TravelResponseDto>(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task FilterTravelAsync_Return_Ok_Result(Mock<ITravelService> travelService,
+                                                               List<TravelResponseDto> expected)
+        {
+            // Arrange
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            TravelFilterDto dto = new TravelFilterDto();
+            travelService.Setup(setup => setup.FilterTravelAsync(dto)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.TravelFilterAsync(dto);
+
+            var apiResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            List<TravelResponseDto> response = model.Data as List<TravelResponseDto>;
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<List<TravelResponseDto>>(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task UpdateTravelAsync_Return_NoContent_Result(Mock<ITravelService> travelService)
+        {
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            TravelUpdateRequestDto dto = new TravelUpdateRequestDto();
+            travelService.Setup(setup => setup.ReplaceTravelAsync(travelId, dto));
+
+            // Act
+            var result = sut.UpdateTravelAsync(travelId,dto);
+
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DeleteTravelAsync_Return_NoContent_Result(Mock<ITravelService> travelService)
+        {
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            travelService.Setup(setup => setup.DeleteTravelAsync(travelId));
+
+            // Act
+            var result = sut.DeleteTravelAsync(travelId);
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+        [Theory, AutoMoqData]
+        public async Task ActiveTravelAsync_Return_NoContent_Result(Mock<ITravelService> travelService)
+        {
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            travelService.Setup(setup => setup.ActiveTravelAsync(travelId));
+
+            // Act
+            var result = sut.ActiveTravelAsync(travelId);
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task PassiveTravelAsync_Return_NoContent_Result(Mock<ITravelService> travelService)
+        {
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            travelService.Setup(setup => setup.PassiveTravelAsync(travelId));
+
+            // Act
+            var result = sut.PassiveTravelAsync(travelId);
+
+            // Assert
+
+            Assert.IsType<NoContentResult>(result.Result);
+            Assert.IsNotType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task IncludeTravelAsync_Return_Ok_Result(Mock<ITravelService> travelService,
+                                                               TravelResponseDto expected)
+        {
+            // Arrange
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            travelService.Setup(setup => setup.IncludeTravelAsync(userId,travelId)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.IncludeTravelAsync(userId,travelId);
+
+            var apiResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            TravelResponseDto response = model.Data as TravelResponseDto;
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<TravelResponseDto>(expected);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task LeaveTravelAsync_Return_Ok_Result(Mock<ITravelService> travelService,
+                                                              TravelResponseDto expected)
+        {
+            // Arrange
+            var sut = new TravelController(travelService.Object);
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            travelService.Setup(setup => setup.LeaveTravelAsync(userId,travelId)).Returns(Task.FromResult(expected));
+
+            // Act
+            var result = sut.LeaveTravelAsync(userId,travelId);
+
+            var apiResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var model = Assert.IsType<ApiResult>(apiResult.Value);
+            TravelResponseDto response = model.Data as TravelResponseDto;
+
+            // Assert
+
+            Assert.IsType<OkObjectResult>(result.Result);
+            Assert.IsNotType<CreatedAtActionResult>(result.Result);
+            Assert.IsNotType<BadRequestObjectResult>(result.Result);
+            Assert.IsNotType<AcceptedAtActionResult>(result.Result);
+
+            Assert.NotNull(result.Result);
+            Assert.NotNull(expected);
+            Assert.IsAssignableFrom<TravelResponseDto>(expected);
+        }
+    }
+}
