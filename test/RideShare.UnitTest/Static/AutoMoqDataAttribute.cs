3a2e2fb3d882f8bd11f6a2b678998a456ac63479
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 18:07:03 +0300

    Add auto moq data

---
+++ test/RideShare.UnitTest/Static/AutoMoqDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RideShare.UnitTest.Static
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute(int count = 3)
+            : base(() => new Fixture { RepeatCount = count, }.Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+}
