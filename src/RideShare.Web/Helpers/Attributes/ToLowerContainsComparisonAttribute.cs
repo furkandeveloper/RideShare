87a15b2ecb1773afd01e1493c9e49d7b47962c97
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-07-18 17:18:33 +0300

    Add attributes for autofilterer

---
+++ src/RideShare.Web/Helpers/Attributes/ToLowerContainsComparisonAttribute.cs
@@ -0,0 +1,30 @@
+﻿using AutoFilterer.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RideShare.Web.Helpers.Attributes
+{
+    public class ToLowerContainsComparisonAttribute : FilteringOptionsBaseAttribute
+    {
+        public override Expression BuildExpression(Expression expressionBody, PropertyInfo targetProperty, PropertyInfo filterProperty, object value)
+        {
+            var containsMethod = typeof(string).GetMethod(nameof(string.Contains), types: new[] { typeof(string) });
+
+            var toLowerMethod = typeof(string).GetMethod(nameof(string.ToLower), types: new Type[0]);
+
+            var comparison = Expression.Equal(
+                        Expression.Call(
+                            method: containsMethod,
+                            instance: Expression.Call(method: toLowerMethod, instance: Expression.Property(expressionBody, filterProperty.Name)
+                                ),
+                            arguments: new[] { Expression.Call(method: toLowerMethod, instance: Expression.Constant(value)) }),
+                        Expression.Constant(true));
+
+            return comparison;
+        }
+    }
+}
